0eb9410db21fcd511f0e46f6120f896bcb7711f7
Alexander Vilchez <a.vilchezr@pucp.pe>
2019-06-16 20:09:59 -0500

    Added writting message

--- index.js
+++ index.js
@@ -5,33 +5,42 @@ var data = require("./data.json");
 
 
 function emit(socket,msg){
-    socket.emit('message', msg);
+    socket.emit('writting',true);
+    setTimeout(()=> {
+        socket.emit('writting',false);
+        socket.emit('message',msg)
+    }                
+    ,2000)
 }
 
 io.on('connection', function(socket){
+    console.log("usuario conectado")
     socket.count=0
-    emit(socket,data.messages[socket.count]);
+    //emit(socket,data.messages[socket.count]);
     socket.on('message', function(msg){
         socket.count++;
         switch (socket.count) {
             case 1:
             case 2:
-                emit(socket,data.messages[socket.count]);
+                emit(socket,data.messages[socket.count].replace("%usuario%",socket.user))
                 break;
             case 3:
-                user = msg.split("saluda a ")[1]?msg.split("saluda a ")[1]:"usuario";
-                emit(socket,data.messages[socket.count].replace("%usuario%",user));
+                user = msg.split("saluda a ")[1]?msg.split("saluda a ")[1]:"invitado";
+                emit(socket,data.messages[socket.count].replace("%invitado%",user))
                 socket.count++;
-                setTimeout(()=> emit(socket,data.messages[socket.count]),5000)
-               
+                setTimeout(()=> emit(socket,data.messages[socket.count]),5000)               
                 break;        
             default:
-                emit(socket,data.messages[5]);
+                socket.emit('message',data.messages[5])
                 break;
         }
     });
+    socket.on('login',function(user){
+        user = user ? user : "usuario"
+        socket.user = user;
+    })
     socket.on('disconnect', function(){
-
+        console.log("usuario desconectado: "+socket.user)
     });
 });
 
